814d8de70aa53330c1aa813cbacc21c2aa0f4b6e
vitor-costa <v2costa@gmail.com>
2014-10-06 22:28:16 -0300

    Testing stegano controller

--- test/controllers/stegano_controller_test.rb
+++ test/controllers/stegano_controller_test.rb
@@ -4,4 +4,26 @@ class SteganoControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+  	get :index
+  	assert_response :success
+  	assert_template :home_page
+  end
+
+  test "download template" do
+  	get :download_tmpl
+  	assert_response :success
+  	assert_template :download_tmpl
+  end
+
+  test "donwload file" do
+  	get :download
+  	assert_equal("application/text", response.content_type)
+  end
+
+  test "download article" do
+  	get :show_article
+  	assert_equal("application/pdf", response.content_type)
+  end
 end
